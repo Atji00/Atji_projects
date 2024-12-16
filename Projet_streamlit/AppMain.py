de803295030386b5f10f15a211b6a51741a615fd
Atji00 <atjicheick@gmail.com>
2024-12-16 12:41:52 +0100

    Update AppMain.py

--- Projet_streamlit/AppMain.py
+++ Projet_streamlit/AppMain.py
@@ -3,12 +3,6 @@
 #===========================================================
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
-from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
